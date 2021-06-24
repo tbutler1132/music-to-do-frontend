3f416ce1b6820f91c7908d1ac1536c819c72e2c8
Timothy Butler <tbutler1132@gmail.com>
2021-06-24 08:41:45 -0400

    final

--- src/redux/actions.js
+++
@@ -1,8 +0,0 @@
-
-
-export const setCurrentUser = (userObj) => {
-    return function (dispatch){
-        console.log("I'm hit")
-        dispatch({type: "set_current_user", payload: userObj})
-    }
-}
